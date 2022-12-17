4ebfd6af57e1f8ed82a55769dc1f2ea963c90694
kkrrissttofferrssonn <christopheralduncan@gmail.com>
2022-12-17 17:09:43 -0500

    1 file modified, 1 deleted

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -12,12 +12,64 @@ const sayHi = () => {
 <template>
   <section>
     <header>
-      <h2>Form</h2>
+      <h2>Add MovementName</h2>
     </header>
     <form @submit.prevent="sayHi">
+      <v-form>
+        <v-container>
+          <v-row>
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field label="Regular"></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field
+                label="Regular"
+                placeholder="Placeholder"
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field label="Solo" solo></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field
+                label="Solo"
+                placeholder="Placeholder"
+                solo
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field label="Filled" filled></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field
+                label="Filled"
+                placeholder="Placeholder"
+                filled
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field label="Outlined" outlined></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field
+                label="Outlined"
+                placeholder="Placeholder"
+                outlined
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
       <v-text-field
         autofocus
-        label="What's your name?"
+        label="Add your movement name here?"
         v-model="name"
       ></v-text-field>
       <v-btn
