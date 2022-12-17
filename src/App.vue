96e4f4e803910b02a4cfae658929fefe2669c2fc
kkrrissttofferrssonn <christopheralduncan@gmail.com>
2022-12-17 17:13:59 -0500

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,26 @@
   <v-app>
     <Header />
     <v-main>
+      <v-container class="px-0" fluid>
+        <v-switch
+          v-model="switch1"
+          :label="`DynamicMovement: ${switch1.toString()}`"
+        ></v-switch>
+      </v-container>
       <Form />
     </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      switch1: true,
+    };
+  },
+};
+</script>
 
 <style scoped>
 .v-main {
