3e8a02fe5c4c9243064989c6769c4c5f6d1ae53c
kkrrissttofferrssonn <christopheralduncan@gmail.com>
2022-12-17 17:19:04 -0500

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,34 @@
   <v-app>
     <Header />
     <v-main>
+      <v-card>
+        <v-layout>
+          <v-navigation-drawer
+            image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+            permanent
+            theme="dark"
+          >
+            <v-list nav>
+              <v-list-item
+                prepend-icon="mdi-email"
+                title="Inbox"
+                value="inbox"
+              ></v-list-item>
+              <v-list-item
+                prepend-icon="mdi-account-supervisor-circle"
+                title="Supervisors"
+                value="supervisors"
+              ></v-list-item>
+              <v-list-item
+                prepend-icon="mdi-clock-start"
+                title="Clock-in"
+                value="clockin"
+              ></v-list-item>
+            </v-list>
+          </v-navigation-drawer>
+          <v-main style="height: 250px"></v-main>
+        </v-layout>
+      </v-card>
       <v-badge>
         <v-icon icon="mdi-vuetify" size="x-large"></v-icon>
       </v-badge>
@@ -30,5 +58,6 @@ export default {
 <style scoped>
 .v-main {
   padding: 1rem 0;
+  background: Green;
 }
 </style>
